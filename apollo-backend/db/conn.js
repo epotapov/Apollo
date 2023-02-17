233708405aabdba7ef62f8a9d149405604e293d6
jebeene <89660618+jebeene@users.noreply.github.com>
2023-02-17 14:51:19 -0500

    added mongoose

--- apollo-backend/db/conn.js
+++ apollo-backend/db/conn.js
@@ -7,28 +7,24 @@
  *
  */
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1
 });
-
+ 
 var _db;
-
+ 
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
-      console.log("db: " + db);
       // Verify we got a good "db" object
-      // if (db)
-      // {
-      db.once('open', () => {
+      if (db)
+      {
         _db = db.db("employees");
         console.log("Successfully connected to MongoDB."); 
-      });
-      // }
+      }
       return callback(err);
          });
   },
--- apollo-backend/server.js
+++ apollo-backend/server.js
@@ -4,6 +4,25 @@
  * @source: https://www.mongodb.com/languages/mern-stack-tutorial
  */
 
+// To connect with your mongoDB database
+// const mongoose = require("mongoose");
+// // Connecting to database
+// mongoose.connect(
+//   "mongodb://localhost:27017/",
+//   {
+//     dbName: "Apollo",
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) =>
+//     err ? console.log(err) : console.log(
+//       "Connected to yourDB-name database")
+// );
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// console.log("App listen at port 5001");
+
 
 const express = require("express");
 const app = express();
