b6f65cabe91f033dd81c5519efa856d10f949dca
jebeene <89660618+jebeene@users.noreply.github.com>
2023-02-17 01:09:26 -0500

    not sure if it connects or not - will work in morning

--- apollo-backend/db/conn.js
+++ apollo-backend/db/conn.js
@@ -14,9 +14,9 @@ const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
--- apollo-backend/server.js
+++ apollo-backend/server.js
@@ -9,7 +9,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
