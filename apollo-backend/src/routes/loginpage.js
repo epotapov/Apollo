5c049a460a800629d65cc620b4e32debfe74c470
Daniel Sowers <danielsowers722@gmail.com>
2023-02-18 14:39:46 -0500

    Coding style

--- apollo-backend/src/routes/loginpage.js
+++ apollo-backend/src/routes/loginpage.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', () => {})
+router.get('/', () => {});
 
-module.exports = router
+module.exports = router;
