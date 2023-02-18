3e2ea9ff4af3322c8ad61964b6b55ba900ee2419
jebeene <89660618+jebeene@users.noreply.github.com>
2023-02-17 22:34:00 -0500

    added dev script

--- apollo-backend/src/server.js
+++ apollo-backend/src/server.js
@@ -13,12 +13,24 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// express app
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 
+// routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'testing 123'})
+})
+
+// listen for requests
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+// connect to MongoDB
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 const connection = mongoose.connection;
@@ -26,6 +38,4 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+
