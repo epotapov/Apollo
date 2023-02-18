609d4da8822000e1b465f80dda7886df42bad7bb
Daniel Sowers <danielsowers722@gmail.com>
2023-02-18 14:48:57 -0500

    Changes to loginpage routes

--- apollo-backend/src/routes/loginpage.js
+++ apollo-backend/src/routes/loginpage.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', () => {});
+//Get a single accounts info
+router.get('/:id', (req, res) => {
+    res.json({mssg: "Get a single accounts info"})
+});
 
 module.exports = router;
