52e15848e84e160bb75313a2ead91d1243f343e7
Daniel Sowers <danielsowers722@gmail.com>
2023-02-18 14:50:37 -0500

    Add a new acc

--- apollo-backend/src/routes/loginpage.js
+++ apollo-backend/src/routes/loginpage.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 //Get a single accounts info
 router.get('/:id', (req, res) => {
-    res.json({mssg: "Get a single accounts info"})
+    res.json({mssg: "Get a single accounts info"});
+});
+
+//Add a new account
+router.post('/', (req, res) => {
+    res.json({mssg: "Add a new account"});
 });
 
 module.exports = router;
