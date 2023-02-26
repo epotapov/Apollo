dfb846dcb9c4c131c6ae97ef2edad9c317a3e878
Edward Potapov <epotapov@comcast.net>
2023-02-25 21:48:22 -0500

    forgot password added to login

--- apollo-frontend/src/components/LoginPage.js
+++ apollo-frontend/src/components/LoginPage.js
@@ -13,85 +13,158 @@ const onFinishFailed = (errorInfo) => {
 
 export default function LoginPage() {
     const [size, setSize] = useState('large');
-    return(
-        <div className='Container'>
-            <Link to='/'>
-                <Button type="primary" className="SignIn" size={size}>
-                    Back
-                </Button>
-            </Link>
-            <Form
-                name="basic"
-                labelCol={{
-                span: 8,
-                }}
-                wrapperCol={{
-                span: 16,
-                }}
-                style={{
-                maxWidth: 600,
-                }}
-                initialValues={{
-                remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <h2>Log In</h2>
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        {
-                        required: true,
-                        message: 'Please input your username!',
-                        },
-                    ]}
+    const [forgotPass, setForgotPass] = useState(false);
+    if (!forgotPass) {
+        return(
+            <div className='Container'>
+                <Link to='/'>
+                    <Button type="primary" className="SignIn" size={size}>
+                        Back
+                    </Button>
+                </Link>
+                <Form
+                    name="basic"
+                    labelCol={{
+                    span: 8,
+                    }}
+                    wrapperCol={{
+                    span: 16,
+                    }}
+                    style={{
+                    maxWidth: 600,
+                    }}
+                    initialValues={{
+                    remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                <Input />
-                </Form.Item>
+                    <h2>Log In</h2>
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please input your username!',
+                            },
+                        ]}
+                    >
+                    <Input />
+                    </Form.Item>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                        required: true,
-                        message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                <Input.Password />
-                </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                    <Input.Password />
+                    </Form.Item>
 
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                    <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+
+                    <Link to='/SignUp'>
+                        <h3>Sign Up</h3>
+                    </Link>
+
+                    <Link>
+                        <h3 onClick={() => setForgotPass(true)}>I forgot my password</h3>
+                    </Link>
+
+                    <Form.Item
                     wrapperCol={{
                         offset: 8,
                         span: 16,
                     }}
-                >
-                <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-
-                <Link to='/SignUp'>
-                        <h3>Sign Up</h3>
-                </Link>
-
-                <Form.Item
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-                >
-                <Button type="primary" htmlType="submit">
-                    Submit
+                    >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className='Container'>
+                <Button type="primary" className="SignIn" onClick={() => setForgotPass(false)} size={size}>
+                    Back
                 </Button>
-                </Form.Item>
-            </Form>
-        </div>
-    );
+                <Form
+                    name="basic"
+                    labelCol={{
+                    span: 8,
+                    }}
+                    wrapperCol={{
+                    span: 16,
+                    }}
+                    style={{
+                    maxWidth: 600,
+                    }}
+                    initialValues={{
+                    remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <h2>Forgot My Password</h2>
+
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
+                    >
+                    <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="New Password"
+                        name="newpassword"
+                        rules={[
+                            {
+                            required: true,
+                            message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                    <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                    >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        )
+    }
 }
