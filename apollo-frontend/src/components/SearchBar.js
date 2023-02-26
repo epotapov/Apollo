7b942f0d14ce331de75018ee13c279dd3f88bd7e
Edward P <epotapov@purdue.edu>
2023-02-26 17:30:13 -0500

    search bar stuff

--- apollo-frontend/src/components/SearchBar.js
+++ apollo-frontend/src/components/SearchBar.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
+import '../App.css'
 
 import { UserOutlined } from '@ant-design/icons';
 import { AutoComplete, Input } from 'antd';
 
-
-/*const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
-const searchResult = (query) =>
-new Array(getRandomInt(5))
-.join('.')
-.split('.')
-.map((_, idx) => {
-    const category = `${query}${idx}`;
-    return {
-        value: category,
-        label: (
-            <div
-            style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-            }}
-            >
-            <span>
-                Found {query} on{' '}
-                <a
-                href={"/Course"}
-                >
-                {category}
-                </a>
-            </span>
-            <span>{getRandomInt(200, 100)} results</span>
-            </div>
-        ),
-    };
-});*/
 
 const renderTitle = (title) => (
     <span>
@@ -43,6 +14,8 @@ const renderTitle = (title) => (
           float: 'right',
         }}
         href="https://www.google.com/search?q=antd"
+        target="_blank"
+        rel="noopener noreferrer"
       >
         more
       </a>
@@ -78,28 +51,12 @@ const options = [
     {
         label: renderTitle('Users'),
         options: [renderItem('AntDesign design language', 100000)],
-    },
+    }
 ];
 
 export default function SearchBar() {
     const [options, setOptions] = useState([]);
     const [size, setSize] = useState('large');
-    /*const handleSearch = (value) => {
-        setOptions(value ? searchResult(value) : []);
-    };
-    const onSelect = (value) => {
-        console.log('onSelect', value);
-    };*/
-    /*return (
-        <AutoComplete
-            dropdownMatchSelectWidth={252}
-            options={options}
-            onSelect={onSelect}
-            onSearch={handleSearch}
-            >
-            <Input.Search size="large" placeholder="input here" enterButton />
-        </AutoComplete>
-    )*/
     return(
         <AutoComplete
             popupClassName="certain-category-search-dropdown"
