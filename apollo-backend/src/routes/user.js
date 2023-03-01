8500fc63b6502de4bbc7ba98e5d6f37137c4d1b6
brandonj1 <77906200+brandonj1@users.noreply.github.com>
2023-02-28 22:52:38 -0500

    small change to edit

--- apollo-backend/src/routes/user.js
+++ apollo-backend/src/routes/user.js
@@ -87,7 +87,7 @@ router.post('/edit', async (req, res) => {
 
     const user = req.body.user;
 
-    const exists = await UserInfo.findOne({ username: decoded.user.username });
+    const exists = await UserInfo.findOne({ username: user.username });
 
     if (!exists) {
         res.send('User does not exist');
