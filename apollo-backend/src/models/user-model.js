25fc90061ea7d1fd1a9cd5086790d08a430bccb3
Daniel Eylem Sowers <danielsowers722@gmail.com>
2023-03-03 01:48:53 -0500

    dfault profile picture

--- apollo-backend/src/models/user-model.js
+++ apollo-backend/src/models/user-model.js
@@ -196,10 +196,7 @@ userSchema.statics.signup = async function(username, email, password, major,
     const salt = await bcrypt.genSalt(10);           //salt adds random string of characters on top of password
     const hashedPassword = await bcrypt.hash(password, salt);       //hashes salt with password
 
-    //Check if a profile pick was not chosen. If not chosen, select the default profile picture.
-    /*if (!profilePicture && profilePicture.length <= 3) {
-        profilePicture = "../../profile_pictures/defaultpfp.png"
-    }*/
+    profilePicture = "../../profile_pictures/defaultpfp.png"
 
     const user = await this.create({username, email, password: hashedPassword, major, gradYear, profilePicture, role, courses, aboutMe, country, gender, planOfStudy, DOB, isPrivate});
 
