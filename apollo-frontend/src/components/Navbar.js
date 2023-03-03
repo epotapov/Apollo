b0242e8175aabf12c2059509e3ee8b4078cb9d09
AliAlDarwish <alialdarwish02@gmail.com>
2023-03-02 23:50:14 -0500

    Added picture for going to profile

--- apollo-frontend/src/components/Navbar.js
+++ apollo-frontend/src/components/Navbar.js
@@ -1,16 +1,23 @@
 import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../img/apollo-gray.png';
-
-import { Button } from 'antd';
+import { Button, Avatar } from 'antd';
 import SearchBar from './SearchBar';
 import { useLogout } from '../hooks/useLogout';
 import { useUserContext } from '../hooks/useUserContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
     const [size, setSize] = useState('large');
     const { logout } = useLogout();
     const { user } = useUserContext();
+    const navigate = useNavigate(); 
+
+    const goToProfile = () => {
+		fetch('http://localhost:5001/api/user/get/' + user.username)
+		.then(response => response.json())
+		.then(data => navigate('/Profile',{state: {user: data}}))
+    }
     return(
         <div id='Navbar'>
             <Link to='/'>
@@ -21,6 +28,7 @@ export default function Navbar() {
                 {user && (
                     <div>
                         <span>Welcome {user.username} </span>
+                        <Avatar onClick={goToProfile} size={50} shape="circle" src="../img/apollo-gray.png" />
                         <Button type="primary" onClick={() => logout()} size={size}>
                             Log Out
                         </Button>
