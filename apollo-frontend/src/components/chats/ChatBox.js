6a72a5d73bb17c3b1bc246df7f5ceee2357ead42
jebeene <89660618+jebeene@users.noreply.github.com>
2023-04-17 23:02:46 -0400

    ui fix

--- apollo-frontend/src/components/chats/ChatBox.js
+++ apollo-frontend/src/components/chats/ChatBox.js
@@ -1,4 +1,3 @@
-import { Box } from "@chakra-ui/layout";
 import { Layout } from "antd";
 import "./styles.css";
 import SingleChat from "./SingleChat";
@@ -10,31 +9,23 @@ const Chatbox = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat } = ChatState();
 
   return (
-    // <Layout style={{ padding: "10px" }}>
-    //   <Content
-    //     style={{
-    //       display: selectedChat ? "flex" : "none",
-    //       alignItems: "center",
-    //       flexDirection: "column",
-    //       backgroundColor: "white",
-    //       width: { base: "100%", md: "68%" },
-    //       borderRadius: "lg",
-    //       borderWidth: "1px",
-    //       marginLeft: { base: 0, md: "10px" },
-    //     }}
-    //   >
-    <Box
-      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
-      alignItems="center"
-      flexDir="column"
-      p={3}
-      bg="white"
-      w={{ base: "100%", md: "68%" }}
-      borderRadius="lg"
-      borderWidth="1px"
-    >
-      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-    </Box>
+    <Layout style={{ padding: "10px" }}>
+      <Content
+        style={{
+          display: selectedChat ? "flex" : "none",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "white",
+          width: { base: "100%", md: "68%" },
+          borderRadius: "lg",
+          borderWidth: "1px",
+          marginLeft: { base: 0, md: "10px" },
+        }}
+      >
+        <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+      </Content>
+    </Layout>
+
   );
 };
 
